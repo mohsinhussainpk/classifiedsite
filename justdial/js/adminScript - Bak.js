9522ab8ad48d65ac7879818fadb6b84b0e9a1b76
Bilal Syed <sbilalahmed95@gmail.com>
2017-07-10 02:55:48 +0500

    Businesses Integrated

--- justdial/js/adminScript - Bak.js
+++ justdial/js/adminScript - Bak.js
@@ -1,15 +1,14 @@
-//TODO:Services Nes Offers
+
 var state = 1;
 var appUrl="Services";
 var rootRef;
 var urlStack = [];
 var modalKey="";
 var modalName="";
-var count=0;
+
 
 urlStack.push(appUrl);
 $(document).ready(function () {
-
     listing();
     $('#deleteModal').on('show.bs.modal', function (event) {
         var button = $(event.relatedTarget); // Button that triggered the modal
@@ -32,28 +31,25 @@ $(document).ready(function () {
 });
 
 function listing(){
-    $('#btnAssignBusiness').hide();
-    console.log("Listing::"+urlStack.join("/"));
+    console.log(urlStack.join("/"));
     rootRef = firebase.database().ref().child(urlStack.join("/"));
     $('#tBody').empty();
+
     rootRef.off();
     rootRef.on("child_added",snap => {
-
         var name=snap.child('name').val();
         var key=snap.key;
         console.log('Added called in Listing');
-        count++;
         var s=  "<tr id='"+key+"' style='cursor: pointer'>"+
                 "<td onclick=\"itemClick('"+key+"')\" type='button'>"+name+"</td>"+
                 "<td>"+
-            "<button type='button' class='btn btn-danger pull-right' style='margin-left:10px' data-toggle='modal' data-whatever='ss' data-target='#deleteModal' onclick=\"setItemToModalKey('"+key+"','"+name+"')\">Delete</button>"+
+            "<button type='button' class='btn btn-danger pull-right' style='margin-left: 10px' data-toggle='modal' data-whatever='ss' data-target='#deleteModal' onclick=\"setItemToModalKey('"+key+"','"+name+"')\">Delete</button>"+
                 "<button type='button' class='btn btn-success pull-right' data-toggle='modal' data-target='#updateModal' onclick=\"setItemToModalKey('"+key+"','"+name+"')\">Update</button>"+
 
             "</td>"+
                 "</tr>";
         $('#tBody').append(s);
     });
-
 
     rootRef.on("child_changed",snap => {
         var name=snap.child('name').val();
@@ -76,108 +72,28 @@ function listing(){
     });
 }
 
-
-
-
 function itemClick(id){
     urlStack.push(id,"categories");
     console.log("AppUrl:"+appUrl+" UrlStack:"+urlStack.join("/"));
-    count=0;
-
     listing();
-    checkLeave();
-
-    console.info(count);
-
-
     console.log("id:"+id+" UrlStackLast:"+urlStack[urlStack.length-2]);
     state++;
-}
-//----------------------------------------Business Code Start-----------------------------------------------------------
-
-function businesslisting(){
-    $('#btnAssignBusiness').hide();
-    console.warn("Business Listing::"+urlStack.join("/"));
-    var businessRef = firebase.database().ref().child("Businesses");
-
-    rootRef = firebase.database().ref().child(urlStack.join("/"));
-    $('#tBody').empty();
-    rootRef.off();
-    rootRef.on("child_added",snap => {
-        //alert(snap.child('xyz').child('bid').val());
-        var bid=snap.val();
-        var key=snap.key;
-        var bName;
-        var bKey;
-        businessRef.on('value', function(snap) {
-            bName=snap.child(bid+"/name").val();
-            bKey=snap.child(bid+"/name").key;
-
-            var s=  "<tr id='"+key+"' style='cursor: pointer'>"+
-                "<td onclick=\"itemClick('"+key+"')\" type='button'>"+bName+"</td>"+
-                "<td>"+
-                "<button type='button' class='btn btn-danger pull-right' style='margin-left: 10px' data-toggle='modal' data-whatever='ss' data-target='#deleteModal' onclick=\"setItemToModalKey('"+key+"','"+name+"')\">Delete</button>"+
-                "<button type='button' class='btn btn-success pull-right' data-toggle='modal' data-target='#updateModal' onclick=\"setItemToModalKey('"+key+"','"+name+"')\">Update</button>"+
-
-                "</td>"+
-                "</tr>";
-            $('#tBody').append(s);
-        });
-
-
-
-
-    });
-
-}
-//----------------------------------------Business Code End-------------------------------------------------------------
-
-
-function checkLeave() {
-    if(count==0){
-        $('#btnAssignBusiness').show();
-        urlStack.pop();
-        urlStack.push("businesses");
-        //TODO:call business listing
-        businesslisting();
-        //(urlStack.join("/"));
-    }else{
-        $('#btnAssignBusiness').hide();
-    }
-
 }
 
 function btnBack() {
     if(state>1){
         urlStack.pop();
         urlStack.pop();
-        //console.log("AppUrl:" + appUrl);
-        //console.log("urlStack:" + urlStack.join("/"));
-        count=0;
-        listing();
-        //checkLeave();
+        console.log("AppUrl:" + appUrl);
+        console.log("urlStack:" + urlStack.join("/"));
+        listing(urlStack[urlStack.length - 2]);
         state--;
-    }
-    else{
-        $('#btnAssignBusiness').hide();
     }
 }
 
 function insert(){
-    var txtItem = $('#txtItem').val();
-    urlStack.pop();
+var txtItem = $('#txtItem').val();
     rootRef.push().set({'name':txtItem});
-}
-
-function insertBusiness(){
-    var txtBusiness = $('#txtBusiness').val();
-    urlStack.pop();
-    urlStack.push('Businesses');
-    //rootRef = firebase.database().ref().child(urlStack.join("/"));
-    rootRef = firebase.database().ref().child("Businesses");
-    rootRef.off();
-    //TODO: Change to business branch
-    rootRef.push().set({'name':txtBusiness});
 }
 
 function setItemToModalKey(id,name){
